a053ba8b7ed0c177f5284835468561faf1f97795
GrantKimball <m263354@usna.edu>
2023-10-19 14:46:10 -0400

    Fishes added

--- game1.py
+++ game1.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 import sys
 
 # initialize pygame
@@ -7,6 +8,7 @@ pygame.init()
 # screen dimensions
 screen_width = 800
 screen_height = 600
+tile_size=64
 
 # define colors
 BLUE = (0, 0, 255)
@@ -17,25 +19,41 @@ pygame.display.set_caption('A beautiful beach')
 
 
 def draw_background(screen):
-    water=pygame.image.load("")
+    water=pygame.image.load("assets/sprites/water.png").convert()
+    sand=pygame.image.load("assets/sprites/sand_top.png").convert()
+    seagrass=pygame.image.load("assets/sprites/seagrass.png").convert()
 
+    sand.set_colorkey((0,0,0))
+    seagrass.set_colorkey((0,0,0))
 
-# main loop
+    #fill screen
+    for x in range(0, screen_width, tile_size):
+        for y in range(0, screen_height, tile_size):
+            screen.blit(water, (x,y))
+
+    #draw sand
+    for x in range(0, screen_width, tile_size):
+        screen.blit(sand, (x,screen_height-tile_size))
+
+    for _ in range(7):
+        x=random.randint(0,screen_width)
+        screen.blit(seagrass, (x, screen_height-tile_size*2))
+
+#main loop
 running = True
+background = screen.copy()
+draw_background(background)
+
 while running:
     for event in pygame.event.get():
-        if event.type==(pygame.QUIT):
+        if event.type == pygame.QUIT:
             running = False
+    #draw background
+    screen.blit(background, (0,0))
 
-    # fill screen with blue
-    screen.fill(BLUE)
-
-    # draw rectangle
-    rectangle_height = 200
-    pygame.draw.rect(screen, BROWN, (0, screen_height - rectangle_height, screen_width, rectangle_height))
-
-    # update display
     pygame.display.flip()
 
-# quit pygame
 pygame.quit()
+
+
+
