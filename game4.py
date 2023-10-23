60b907ab93d39c457f3661ff49895f3714e741d5
GrantKimball <m263354@usna.edu>
2023-10-23 15:22:06 -0400

    fish moving

--- game4.py
+++ game4.py
@@ -13,10 +13,10 @@ tile_size=64
 
 #create screen
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('A beautiful beach')
+pygame.display.set_caption('A School of Moving Fish')
 
-#load font into game
-custom_font=pygame.font.Font("assets/fonts/electrical.ttf", 50)
+#clock object
+clock=pygame.time.Clock()
 
 def draw_background(surf):
     water=pygame.image.load("assets/sprites/water.png").convert()
@@ -41,6 +41,8 @@ def draw_background(surf):
         surf.blit(seagrass, (x, screen_height-tile_size*2))
 
     #draw text
+    # load font into game
+    custom_font = pygame.font.Font("assets/fonts/electrical.ttf", 50)
     text=custom_font.render('Chomp', True, (255,0,0))
     surf.blit(text, (screen_width/2-text.get_width()/2, screen_height-550-text.get_height()/2))
 
@@ -48,6 +50,7 @@ def draw_background(surf):
 running = True
 background = screen.copy()
 draw_background(background)
+
 
 #draw fish
 for _ in range(5):
@@ -57,12 +60,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     #draw background
     screen.blit(background, (0,0))
+    #update fish
+    fishes.update()
+    #check if fish is off screen
+    for fish in fishes:
+        if fish.rect.x < -fish.rect.width: #or tile_size
+            fishes.remove(fish)#remove fish from sprite group
+            fishes.add(Fish(random.randint(0, screen_width +50), random.randint(0, screen_height - 2 * tile_size)))
 
-    #draw sprite group fishes
-    fishes.draw(background)
+    fishes.draw(screen)
 
     pygame.display.flip()
+
+    #limit frame rate
+    clock.tick(60)
+
 
 pygame.quit()
